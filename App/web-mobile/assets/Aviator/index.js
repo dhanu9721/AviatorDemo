af7f6616e9bbc25302c530ffa0b8e8e088b059d1
Dhananjay Maurya <91829244+dhanu9721@users.noreply.github.com>
2025-05-26 00:36:52 +0530

    initial commit

---
+++ App/web-mobile/assets/Aviator/index.js
@@ -0,0 +1,20 @@
+System.register("chunks:///_virtual/Aviator",[],(function(){return{execute:function(){}}}));
+
+(function(r) {
+  r('virtual:///prerequisite-imports/Aviator', 'chunks:///_virtual/Aviator'); 
+})(function(mid, cid) {
+    System.register(mid, [cid], function (_export, _context) {
+    return {
+        setters: [function(_m) {
+            var _exportObj = {};
+
+            for (var _key in _m) {
+              if (_key !== "default" && _key !== "__esModule") _exportObj[_key] = _m[_key];
+            }
+      
+            _export(_exportObj);
+        }],
+        execute: function () { }
+    };
+    });
+});
